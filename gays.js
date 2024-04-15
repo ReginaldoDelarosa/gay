436828dcadd21509a8bf42ad5253a1cb62c736f7
rdelarosa <rdelarosa@gbm.net>
2024-04-15 14:23:34 -0500

    2

--- gays.js
+++ gays.js
@@ -1,45 +1,46 @@
+const fs = require('fs');
+const prompt = require('prompt-sync') ();
 
-const gays=[{
-    id:0,
-    nombre: 'Andres',
-    gay_level:1
-},
-{
-    id:1,
-    nombre: 'Reginaldo',
-    gay_level:0
-}
-,
-{
-    id:2,
-    nombre: 'guillermo',
-    gay_level:0
-}
-];
+let gaysInfo = fs.readFileSync('gays.json','utf-8');
 
+const gays= JSON.parse(gaysInfo);
+console.log(gays)
+
+let gayAdd= prompt('Quiere añadir un nuevo integrante? 1: si o 2: no: ');
+
+
+
+if(gayAdd==1){
+    nombrei=prompt('Ingrese el nombre del nuevo integrante: ')
+    gay_leveli=prompt('Ingrese el nivel de gay del nuevo integrante: ');
+    let nuevoGay=
+    {
+        id: gays.length,
+        nombre: nombrei,
+        gay_level: gay_leveli
+    }
+    gays.push(nuevoGay);
+    let jsonStr = JSON.stringify(gays);
+
+    fs.writeFileSync('gays.json', jsonStr);
+
+}
 
 let reuniones = prompt('Ingrese el numero de reuniones que se tendran al mes: ' );
 
 for (let i=0; i<reuniones; i++){
     let gay= prompt('Ingrese el nombre del integrante del grupo: ');
-    if (gay == 'Andres'){
-        gays[1].gay_level= gays[1].gay_level+1;
-        gays[2].gay_level= gays[1].gay_level+1;
+
+    for(let j=0; j<=gays.length-1;j++)
+    if (gays[j].nombre !==gay){
+        gays[j].gay_level++;
     }
     
-    if(gay=='Reginaldo' && gays[1].gay_level>0){
-        gays[2].gay_level= gays[1].gay_level+1;
-        gays[0].gay_level= gays[1].gay_level+1;
-    }
     
-    if(gay=='guillermo' && gays[2].gay_level>0){
-        gays[1].gay_level= gays[1].gay_level+1;
-        gays[0].gay_level= gays[1].gay_level+1;
-    }
 }
 
-for(let i=0; i<=3; i++){
-    alert(`El nivel de gay de ${gays[i].nombre} es: ${gays[i].gay_level}` )
+for(let i=0; i<=gays.length-1; i++){
+   console.log(`El nivel de gay de ${gays[i].nombre} es: ${gays[i].gay_level}` )
 }
 
 
