e2935529dcb686b938313f07588d81c16ecda080
rdelarosa <rdelarosa@gbm.net>
2024-04-18 17:22:52 -0500

    Codigo sin front

--- gays.js
+++ gays.js
@@ -1,48 +1,45 @@
-const fs = require('fs');
-const prompt = require('prompt-sync') ();
+const fs = require("fs");
+const prompt = require("prompt-sync")();
 
-let gaysInfo = fs.readFileSync('gays.json','utf-8');
+let gaysInfo = fs.readFileSync("gays.json", "utf-8");
 
-const gays= JSON.parse(gaysInfo);
-console.log(gays)
+const gays = JSON.parse(gaysInfo);
+console.log(gays);
 
-let gayAdd= prompt('Quiere añadir un nuevo integrante? 1: si o 2: no: ');
+let gayAdd = prompt("Quiere añadir un nuevo integrante? 1: si o 2: no: ");
 
+if (gayAdd == 1) {
+  nombrei = prompt("Ingrese el nombre del nuevo integrante: ");
+  gay_leveli = prompt("Ingrese el nivel de gay del nuevo integrante: ");
+  let nuevoGay = {
+    id: gays.length,
+    nombre: nombrei,
+    gay_level: gay_leveli,
+  };
+  gays.push(nuevoGay);
+  let jsonStr = JSON.stringify(gays);
 
-
-if(gayAdd==1){
-    nombrei=prompt('Ingrese el nombre del nuevo integrante: ')
-    gay_leveli=prompt('Ingrese el nivel de gay del nuevo integrante: ');
-    let nuevoGay=
-    {
-        id: gays.length,
-        nombre: nombrei,
-        gay_level: gay_leveli
-    }
-    gays.push(nuevoGay);
-    let jsonStr = JSON.stringify(gays);
-
-    fs.writeFileSync('gays.json', jsonStr);
-
+  fs.writeFileSync("gays.json", jsonStr);
 }
 
-let reuniones = prompt('Ingrese el numero de reuniones que se tendran al mes: ' );
+let reuniones = prompt(
+  "Ingrese el numero de reuniones que se tendran al mes: "
+);
 
-for (let i=0; i<reuniones; i++){
-    let gay= prompt('Ingrese el nombre del integrante del grupo: ');
+for (let i = 0; i < reuniones; i++) {
+  let gay = prompt("Ingrese el nombre del integrante del grupo: ");
+  let gayMember = gays.find(member => member.nombre === gay);
 
-    for(let j=0; j<=gays.length-1;j++)
-    if (gays[j].nombre !==gay){
+  // Si el integrante ingresado tiene un nivel de gay mayor que 0, incrementar el nivel de gay de todos los demás
+  if (gayMember && gayMember.gay_level > 0) {
+    for (let j = 0; j <= gays.length - 1; j++) {
+      if (gays[j].nombre !== gay) {
         gays[j].gay_level++;
+      }
     }
-    
-    
+  }
 }
 
-for(let i=0; i<=gays.length-1; i++){
-   console.log(`El nivel de gay de ${gays[i].nombre} es: ${gays[i].gay_level}` )
+for (let i = 0; i <= gays.length - 1; i++) {
+  console.log(`El nivel de gay de ${gays[i].nombre} es: ${gays[i].gay_level}`);
 }
-
-
-
-    
